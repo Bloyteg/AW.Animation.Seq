a78ba76a8af630c611a3e1860915541a7ee87c4f
Mr. Byte <bytemr@gmail.com>
2014-06-19 20:18:06 -0600

    More work on the SEQ loader.

---
+++ Bloyteg.AW.Animation.Seq.Test/BinarySeqTests.fs
@@ -0,0 +1,13 @@
+﻿module Bloyteg.AW.Animation.Seq.Test.BinarySeqTests
+open Bloyteg.AW.Animation.Seq
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``Binary sequences with header 0x7F7F7F7A can be loaded`` =
+    let loader = Loader()
+    loader.LoadFromStream(null)
+
+[<Fact>]
+let ``Binary sequences with header 0x7F7F7F79 can be loaded`` =
+    Assert.True(false)
