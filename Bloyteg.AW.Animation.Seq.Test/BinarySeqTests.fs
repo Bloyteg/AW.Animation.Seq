3c9be1e20964f8019dd6abda5670c4ecfeaef5d3
Mr. Byte <bytemr@gmail.com>
2014-06-20 23:08:59 -0600

    More work on binary animation loading.

--- Bloyteg.AW.Animation.Seq.Test/BinarySeqTests.fs
+++ Bloyteg.AW.Animation.Seq.Test/BinarySeqTests.fs
@@ -7,8 +7,17 @@ open System.IO
 
 [<Fact>]
 let ``Binary sequences with header 0x7F7F7F7A can be loaded`` () =
-    (Loader().LoadFromStream(File.OpenRead("maca.seq")) |> ignore) |> should not' (throw typeof<System.Exception>)
+    use file = File.OpenRead("maca.seq")
+    (Loader().LoadFromStream(file) |> ignore) |> should not' (throw typeof<System.Exception>)
 
 [<Fact>]
 let ``Binary sequences with header 0x7F7F7F79 can be loaded`` () =
-    (Loader().LoadFromStream(File.OpenRead("walk.seq")) |> ignore) |> should not' (throw typeof<System.Exception>)
+    use file = File.OpenRead("walk.seq")
+    (Loader().LoadFromStream(file) |> ignore) |> should not' (throw typeof<System.Exception>)
+
+
+[<Fact>]
+let ``Walk.seq should have total frame count of 34`` () =
+    use file = File.OpenRead("walk.seq")
+    let animation = Loader().LoadFromStream(file) 
+    animation.FrameCount |> should equal 34
--- Bloyteg.AW.Animation.Seq/Loader.fs
+++ Bloyteg.AW.Animation.Seq/Loader.fs
@@ -11,38 +11,40 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 namespace Bloyteg.AW.Animation.Seq
 
-module internal SeqLoader =
-    let readInt16 (stream: System.IO.Stream): int =
-        (stream.ReadByte() <<< 8) ||| (stream.ReadByte())
-
-    let readInt32 (stream: System.IO.Stream): int =
+module internal SeqLoader = 
+    let readInt16 (stream : System.IO.Stream) : int = (stream.ReadByte() <<< 8) ||| (stream.ReadByte())
+    let readInt32 (stream : System.IO.Stream) : int = 
         (stream.ReadByte() <<< 24) ||| (stream.ReadByte() <<< 16) ||| (stream.ReadByte() <<< 8) ||| (stream.ReadByte())
-
-    let readString (stream: System.IO.Stream) length =
-        let stringBytes: byte array = Array.zeroCreate length
+    
+    let readString (stream : System.IO.Stream) length = 
+        let stringBytes : byte array = Array.zeroCreate length
         stream.Read(stringBytes, 0, length) |> ignore
-        System.Text.ASCIIEncoding.ASCII.GetString(stringBytes)
-
-    let loadBinarySeqFromStream stream: Animation = { FramesPerSecond = 30; FrameCount = 0; Joints = Seq.empty }
-
-    let loadTextSeqFromStream stream: Animation = failwith "Not implemented."
-
-    let (|Binary|Text|Unknown|) (stream: System.IO.Stream) =
+        System.Text.ASCIIEncoding.ASCII.GetString(stringBytes).TrimEnd(char 0s)
+    
+    let loadBinarySeqFromStream stream : Animation = 
+        let frameCount = readInt16 stream
+        let jointCount = readInt32 stream
+        let modelNameLength = readInt16 stream
+        let modelName = readString stream modelNameLength
+        { FramesPerSecond = 30
+          FrameCount = frameCount
+          Joints = Seq.empty }
+    
+    let loadTextSeqFromStream stream : Animation = failwith "Not implemented."
+    
+    let (|Binary|Text|Unknown|) (stream : System.IO.Stream) = 
         match stream |> readInt32 with
         | (0x7F7F7F79) | (0x7F7F7F7A) -> Binary
         | (0x41575351) -> Text
         | _ -> Unknown
-
-    let loadFromStream (stream: System.IO.Stream) = 
+    
+    let loadFromStream (stream : System.IO.Stream) = 
         match stream with
         | Binary -> loadBinarySeqFromStream stream
         | Text -> loadTextSeqFromStream stream
         | Unknown -> failwith "Unrecognized file type."
 
-type Loader() =
-    member this.LoadFromStream(stream: System.IO.Stream) =
-        SeqLoader.loadFromStream stream
-
+type Loader() = 
+    member this.LoadFromStream(stream : System.IO.Stream) = SeqLoader.loadFromStream stream
