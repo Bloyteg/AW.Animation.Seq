34d76ab5b5a28fcb08da188a7700f5340f85d1f8
Mr. Byte <bytemr@gmail.com>
2014-06-21 14:33:06 -0600

    Add unit tests to ensure the quaternions being returned are the expected quaternions.

--- Bloyteg.AW.Animation.Seq.Test/BinarySeqTests.fs
+++ Bloyteg.AW.Animation.Seq.Test/BinarySeqTests.fs
@@ -31,3 +31,31 @@ let ``Maca.seq's first joint should be pelvis`` () =
 let ``Maca.seq's first joint has 88 keyframes`` () =
     let animation = loadAnimationFromFile "maca.seq"
     animation.Joints.Head.Keyframes |> should haveLength 88
+
+[<Test>]
+let ``Maca.seq's first joint's first keyframe should be 0`` () =
+    let animation = loadAnimationFromFile "maca.seq"
+    let (firstKeyframe :: _) = animation.Joints.Head.Keyframes
+    firstKeyframe.Keyframe |> should equal 0
+
+[<Test>]
+let ``Maca.seq's first joint's first keyframe should have rotation W = 1.0, X = 0, Y = 0, Z = 0`` () =
+    let animation = loadAnimationFromFile "maca.seq"
+    let (firstKeyframe :: _) = animation.Joints.Head.Keyframes
+    firstKeyframe.Rotation |> should equal { W = 1.0f; X = 0.0f; Y = 0.0f; Z = 0.0f }
+
+[<Test>]
+let ``Maca.seq's first joint's first keyframe should be 2`` () =
+    let animation = loadAnimationFromFile "maca.seq"
+    let (_ :: secondKeyFrame :: _) = animation.Joints.Head.Keyframes
+    secondKeyFrame.Keyframe |> should equal 2
+
+
+[<Test>]
+let ``Maca.seq's first joint's second keyframe should have rotation W = 0.99932152, X = 0, Y = 0.03683079, Z = 0`` () =
+    let animation = loadAnimationFromFile "maca.seq"
+    let (_ :: secondKeyFrame :: _) = animation.Joints.Head.Keyframes
+    secondKeyFrame.Rotation.W |> should (equalWithin 0.00000001) 0.99932152;
+    secondKeyFrame.Rotation.X |> should equal 0.0f
+    secondKeyFrame.Rotation.Y |> should (equalWithin 0.00000001) 0.03683079f
+    secondKeyFrame.Rotation.Z |> should equal 0.0f
