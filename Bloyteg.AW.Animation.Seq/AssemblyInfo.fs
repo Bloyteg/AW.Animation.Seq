c5c35d004a6102eff13e485ab7c16795b898eea7
Mr. Byte <bytemr@gmail.com>
2014-06-21 19:24:16 -0600

    Added NuGet packaging information.

---
+++ Bloyteg.AW.Animation.Seq/AssemblyInfo.fs
@@ -0,0 +1,10 @@
+﻿module AssemblyInfo
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Bloyteg.AW.Animation.Seq")>]
+[<assembly: AssemblyCopyright("Copyright (c) 2014 Joshua R. Rodgers")>]
+[<assembly: AssemblyVersion("1.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0")>]
+[<assembly: Guid("BE3D4280-0A1D-4A72-889A-3BEC92B7CD82")>]
+do ()
